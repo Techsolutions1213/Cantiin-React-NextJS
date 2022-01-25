b60706a7e0623f8c3e1901c2068038fcfc8ce502
myowngit1213 <cavin061213@outlook.com>
2022-01-25 17:12:47 +0200

    EsLint up and running

--- src/src/App.js
+++ src/src/App.js
@@ -1,28 +1,28 @@
-import * as React from 'react';
+import * as React from "react";
 import { Routes, Route,      Link } from "react-router-dom";
-import Drawer from './Custom/Drawer';
+import Drawer from "./Custom/Drawer";
 
 /* Routes */
-import Home from './Routes/Home';
-import About from './Routes/About';
-import Login from './Routes/Login';
-import SignUp from './Routes/Signup';
-import CreateProduct from './Routes/Products/CreateProduct';
-import ProductsList from './Routes/Products/ProductsList';
+import Home from "./Routes/Home";
+import About from "./Routes/About";
+import Login from "./Routes/Login";
+import SignUp from "./Routes/Signup";
+import CreateProduct from "./Routes/Products/CreateProduct";
+import ProductsList from "./Routes/Products/ProductsList";
 
 const App = ()=>{
-  const DrawerContent = (        
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about/" element={<About />} />
-    <Route path="/login/" element={<Login />} />
-    <Route path="/signup/" element={<SignUp />} />
-    <Route path="/products/create/" element={<CreateProduct />} />
-    <Route path="/products/" element={<ProductsList />} />
-    </Routes>);
+	const DrawerContent = (        
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/about/" element={<About />} />
+			<Route path="/login/" element={<Login />} />
+			<Route path="/signup/" element={<SignUp />} />
+			<Route path="/products/create/" element={<CreateProduct />} />
+			<Route path="/products/" element={<ProductsList />} />
+		</Routes>);
 
-    return (<Drawer DrawerContent={DrawerContent} />)
-}
+	return (<Drawer DrawerContent={DrawerContent} />);
+};
 
 
 export default App;
