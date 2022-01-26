ba3b022053ff1ace4a96ad94c2025ba003b6eb22
myowngit1213 <cavin061213@outlook.com>
2022-01-26 16:00:20 +0200

    used old products list

--- Cantiin-React-MaterialUI/src/Routes/Products/ProductsList.js
+++ Cantiin-React-MaterialUI/src/Routes/Products/ProductsList.js
@@ -1,12 +1,116 @@
-import React from "react";
-import Typography from "@mui/material/Typography";
+import { Component, Fragment, useState, useEffect } from "react";
+import {getItemsList, getAllResultsNumber,
+	getCurrentWindowPage,getCurrentResponsePage,
+	getApiResponsePage,getMaxPage} from "../../functions/fetching/list";
+import Pagination from "../../components/pagination";
+import ProductCard from "../../components/cards/product";
 
-export default function ProductsList() {
+
+import fetchers from "../../helpers/fetchers";
+
+const ProductsList = () => {
+    
+	const initialState = {
+		"loaded":false,
+		"items":[],
+		"response":null,
+		"success":false,
+		"err":null
+	};
+
+
+    
+	const [state,setState] = useState(initialState);
+
+	const handleSucessfulResponse = (response) =>
+	{
+		//console.log(response);
+        
+		setState(
+			{
+				...state,
+				"loaded":true,
+				"items":getItemsList(response),
+				"response": response,
+				"success":true,
+				"err":null
+			});
+		//console.log(state);
+	};
+    
+
+	const handleFailingResponse = (err) =>
+	{
+
+		/*console.log(err);
+        console.log(err.toString());
+        console.log(err.response);*/
+        
+		let error = "Something went wrong";
+		try {
+			error=err.response.data.detail;
+			if(error== undefined)
+			{error="Something went wrong";}
+		} catch (error) {
+		}
+        
+		setState({
+			...state,
+			"loaded":true,
+			"items":[],
+			"response":null,
+			"success":false,
+			"err":<div className="message-of-error-page">{error}</div>
+		}
+		);
+	};
+
+
+
+	useEffect(()=>{
+		setState(initialState);
+		fetchers.products.list(getCurrentWindowPage())
+			.then((response)=>{handleSucessfulResponse(response);})
+			.catch((err)=>{handleFailingResponse(err);});       
+	},[window.location.href]);
+    
+    
+	if (!state.loaded)
+	{
+
+		return (
+			<div>
+				<h1>Products List:</h1>
+				<div>Loading...</div>
+			</div>
+		);
+	}
+    
+	if(state.success)
+	{
+		let productsList = state.items.map(product=>{return(
+        
+			<ProductCard key={product.id} item={product}/>
+		);}
+		);
+    
+		return (
+			<div>
+				<h1>Products List:</h1>
+				<Pagination response={state.response}/>
+				<ul className="ProductsList">
+					{productsList}
+				</ul>
+			</div>
+		);
+	}
+
 	return (
-		<>
-			<Typography paragraph>
-    Products List
-			</Typography>
-		</>
+		<div>
+			<h1>Products List:</h1>
+			<div>{state.err}</div>
+		</div>
 	);
-}
+};
+ 
+export default ProductsList;
