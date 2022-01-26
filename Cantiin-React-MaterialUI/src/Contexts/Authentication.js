fadea7ef75bc48dac5e93a7d396c12ef1a8d759a
myowngit1213 <cavin061213@outlook.com>
2022-01-26 15:52:26 +0200

    Copied Auth Context from Canttin-React

---
+++ Cantiin-React-MaterialUI/src/Contexts/Authentication.js
@@ -0,0 +1,48 @@
+import {createContext, useState, useEffect} from "react";
+import fetchers from "../helpers/fetchers";
+
+const AuthContext = createContext();
+
+export default AuthContext;
+
+const AuthContextProvider = (props) => {
+	//console.log(localStorage.getItem("authState"))
+	let defaultAuthState= 
+        (localStorage.getItem("authState")!==null) 
+        	? JSON.parse(localStorage.getItem("authState")) 
+        	: { "is_authenticated":false, "user":null} ;
+
+	const [authState, setAuthState] = useState( defaultAuthState );
+
+
+    
+	const successfulUserResponse = (response) =>{
+		let correctAuthState = {"is_authenticated":true,"user":response.data};
+		localStorage.setItem("authState",JSON.stringify(correctAuthState));
+		setAuthState(correctAuthState);       
+	};
+
+
+	const failureUserResponse = () =>{
+		let correctAuthState = {"is_authenticated":false, "user":null};
+		localStorage.setItem("authState",JSON.stringify(correctAuthState));
+		setAuthState(correctAuthState);      
+	};
+
+    
+	const refetchIsAuthenticated = () =>{
+		fetchers.auth.who()
+			.then((response)=>{successfulUserResponse(response);})
+			.catch((err)=>{failureUserResponse();});
+	};
+    
+	useEffect(()=>{refetchIsAuthenticated();},[localStorage.getItem("authState")]);
+
+	return ( 
+		<AuthContext.Provider 
+			value={{...authState, refetchIsAuthenticated, successfulUserResponse, failureUserResponse}}>
+			{props.children}
+		</AuthContext.Provider> );
+};
+ 
+export {AuthContextProvider};
