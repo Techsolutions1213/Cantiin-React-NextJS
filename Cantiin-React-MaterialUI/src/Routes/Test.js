a318b21ca84fc023d017387d12c7abe38fbfd7ba
myowngit1213 <cavin061213@outlook.com>
2022-01-27 18:40:44 +0200

    Styled the place of error message

--- Cantiin-React-MaterialUI/src/Routes/Test.js
+++ Cantiin-React-MaterialUI/src/Routes/Test.js
@@ -1,62 +1,118 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
+import React,  {useState} from "react";
+import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-export default function ButtonAppBar() {
+function Copyright(props) {
 	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position="static">
-				<Toolbar>
-					<IconButton
-						size="large"
-						edge="start"
-						color="inherit"
-						aria-label="menu"
-						sx={{ mr: 2 }}
-					>
-						<MenuIcon />
-					</IconButton>
-					<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-					</Typography>
-					<Button color="inherit">Login</Button>
-				</Toolbar>
-			</AppBar>
-		</Box>
+		<Typography variant="body2" color="text.secondary" align="center" {...props}>
+			{"Copyright © "}
+			<Link color="inherit" href="https://mui.com/">
+        Your Website
+			</Link>{" "}
+			{new Date().getFullYear()}
+			{"."}
+		</Typography>
 	);
 }
 
+const theme = createTheme();
+
+export default function SignIn() {
 
 
+	const [errMessage,setErrorMessage] = useState("");
 
 
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+		// eslint-disable-next-line no-console
+		console.log({
+			email: data.get("email"),
+			password: data.get("password"),
+		});
+	};
 
-
-
-
-
-
-/*import React from "react";
-
-
-
-const TestRoute = (props) => {
-    
-    
-	return <div>
-
-		<h1>  
-      Test
-		</h1>
-
-	</div>;
-};
- 
-export default TestRoute;
-
-*/
+	return (
+		<ThemeProvider theme={theme}>
+			<Container component="main" maxWidth="xs">
+				<CssBaseline />
+				<Box
+					sx={{
+						marginTop: 8,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+						<LockOutlinedIcon />
+					</Avatar>
+					<Typography component="h1" variant="h5">
+            Sign in
+					</Typography>
+					<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							id="email"
+							label="Email Address"
+							name="email"
+							autoComplete="email"
+							autoFocus
+						/>
+						<TextField
+							margin="normal"
+							required
+							fullWidth
+							name="password"
+							label="Password"
+							type="password"
+							id="password"
+							autoComplete="current-password"
+						/>
+						<Typography component="h1" variant="h5" sx={{
+							display:"flex",
+							justifyContent:"center",
+						}}>
+            Sign in
+						</Typography>
+						<Button
+							type="submit"
+							fullWidth
+							variant="contained"
+							sx={{ mt: 3, mb: 2 }}
+						>
+              Sign In
+						</Button>
+						<Grid container>
+							<Grid item xs>
+								<Link href="#" variant="body2">
+                  Forgot password?
+								</Link>
+							</Grid>
+							<Grid item>
+								<Link href="#" variant="body2">
+									{"Don't have an account? Sign Up"}
+								</Link>
+							</Grid>
+						</Grid>
+					</Box>
+				</Box>
+				<Copyright sx={{ mt: 8, mb: 4 }} />
+			</Container>
+		</ThemeProvider>
+	);
+}
