58a790065f7571c986d0a08242d26c85bc3ddb78
myowngit1213 <cavin061213@outlook.com>
2022-01-28 17:11:45 +0200

    Custom dummy Card works

--- Cantiin-React-MaterialUI/src/Routes/Test.js
+++ Cantiin-React-MaterialUI/src/Routes/Test.js
@@ -1,12 +1,36 @@
 import * as React from 'react';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Container } from '@mui/material';
 
-export default function PaginationRounded() {
+export default function ImgMediaCard() {
   return (
-    <Stack spacing={2}>
-      <Pagination count={10} shape="rounded" />
-      <Pagination count={10} variant="outlined" shape="rounded" />
-    </Stack>
+    <Card sx={{ backgroundColor:"success" }}>
+
+      <CardContent>
+        
+        
+        <div style={{display:"flex"}}>
+        <Typography gutterBottom variant="h4" component="div" sx={{flexGrow:1}}>
+          Product Title
+        </Typography>
+        <Typography gutterBottom variant="h6" component="div" sx={{color:"green"}}>
+          in Stock
+        </Typography>
+        </div>
+
+        <Typography variant="body1" color="text.secondary" sx={{color:"darkgreen", fontWeight:"bold"}}>
+          Price
+        </Typography>
+
+        
+
+      </CardContent>
+
+    </Card>
   );
 }
