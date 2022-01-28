709c1c36f6bc1497fe74a2871d6f1778ef2084f0
myowngit1213 <cavin061213@outlook.com>
2022-01-28 17:19:18 +0200

    Card Text Replaced Successfully

--- Cantiin-React-MaterialUI/src/Components/cards/product.js
+++ Cantiin-React-MaterialUI/src/Components/cards/product.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { Component, useState } from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -7,7 +7,17 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Container } from '@mui/material';
 
-export default function ProductCard() {
+export default function ProductCard(props) {
+    const [state, setState] = useState({"product":props.item});
+    let productCardClassName= "productCard productCardOutOfStock";
+    let inStockText = "Out Of Stock"; 
+    let inStockStyle = "productCard-Element productCard-productInStock productCard-productInStock-Out";
+    if(state.product.in_stock){
+        productCardClassName= "productCard productCardInStock";
+        inStockText = "In Stock";
+        inStockStyle = "productCard-Element productCard-productInStock productCard-productInStock-In";
+    }
+
   return (
     <Card sx={{mb:3}}>
 
@@ -16,15 +26,15 @@ export default function ProductCard() {
         
         <div style={{display:"flex"}}>
         <Typography gutterBottom variant="h4" component="div" sx={{flexGrow:1}}>
-          Product Title
+          {state.product.name}
         </Typography>
         <Typography gutterBottom variant="h6" component="div" sx={{color:"green"}}>
-          in Stock
+            {inStockText}
         </Typography>
         </div>
 
         <Typography variant="body1" color="text.secondary" sx={{color:"darkgreen", fontWeight:"bold"}}>
-          Price
+        ${state.product.price}
         </Typography>
       </CardContent>
 
