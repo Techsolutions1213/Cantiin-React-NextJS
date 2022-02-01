24a20213b53bc6af2378cb88b805fa0e1b58ed54
myowngit1213 <cavin061213@outlook.com>
2022-02-01 17:11:36 +0200

    Updated test route

--- Cantiin-React-MaterialUI/src/Routes/Test.js
+++ Cantiin-React-MaterialUI/src/Routes/Test.js
@@ -1,33 +1,20 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { Container } from '@mui/material';
 
-export default function ImgMediaCard() {
-  return (
-    <Card >
 
-      <CardContent >
-        
-        
-        <div style={{display:"flex"}}>
-        <Typography gutterBottom variant="h4" component="div" sx={{flexGrow:1}}>
-          Product Title
-        </Typography>
-        <Typography gutterBottom variant="h6" component="div" sx={{color:"green"}}>
-          in Stock
-        </Typography>
-        </div>
+ class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
 
-        <Typography variant="body1" color="text.secondary" sx={{color:"darkgreen", fontWeight:"bold"}}>
-          Price
-        </Typography>
-      </CardContent>
-
-    </Card>
-  );
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
+
+export default Clock;
