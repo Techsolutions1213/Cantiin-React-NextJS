023c95bd40d77ae6f43cd328fb8725c574523c16
myowngit1213 <cavin061213@outlook.com>
2022-03-01 18:05:41 +0200

    Set up page and layout

--- Cantiin-React-MaterialUI/src/components/Layout.tsx
+++ Cantiin-React-MaterialUI/src/components/Layout.tsx
@@ -3,11 +3,9 @@ import React from "react";
 export default function Layout({ children }) {
     return (
       <>
-        <main>
-            <div>Header</div>
-                {children}
-            <div>Footer</div>
-        </main>
+        <div>Header</div>
+            {children}
+        <div>Footer</div>
       </>
     )
   }
--- Cantiin-React-MaterialUI/src/pages/index.tsx
+++ Cantiin-React-MaterialUI/src/pages/index.tsx
@@ -1,47 +1,12 @@
-import { AmplifyAuthenticator } from '@aws-amplify/ui-react'
-import { Amplify, API, Auth, withSSRContext } from 'aws-amplify'
-import Head from 'next/head'
-import awsExports from '../aws-exports'
-import { GRAPHQL_AUTH_MODE } from '@aws-amplify/api'
-import { useRouter } from 'next/router'
-import { GetServerSideProps } from 'next'
-//import styles from '../styles/Home.module.css'
-
-Amplify.configure({ ...awsExports, ssr: true })
+import React from "react";
 
 export default function Home({  }) {
-  const router = useRouter()
 
   return (
-    <div>
-      <Head>
-        <title>Amplify + Next.js</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main >
-        <h1 >Amplify + Next.js</h1>
-
-        <p >
-          Todos
-        </p>
-
-        <div >
-          <div >
-            <h3>New Todo</h3>
-          </div>
-        </div>
-      </main>
-    </div>
+    <p>
+      Hi
+    </p>
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
 
-
-
-  return {
-    props: {
-    },
-  }
-}
