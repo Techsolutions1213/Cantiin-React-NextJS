cd73f5125cb878d69e12a1723846e92a93b7dd52
myowngit1213 <cavin061213@outlook.com>
2022-03-02 16:49:48 +0200

    Created productObject Type

--- Cantiin-React-MaterialUI/src/types.tsx
+++ Cantiin-React-MaterialUI/src/types.tsx
@@ -5,6 +5,9 @@ export type creatingPageComponent = JSX.Element & { pageHeader?: string };
 export type pageComponent = JSX.Element & { pageHeader: string };
 
 
+export type productObject = {id:number, in_stock:boolean, name:string, price:number};
+
+
 
 
 
