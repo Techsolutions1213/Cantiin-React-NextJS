b84705f631d0885ea6d950179cf835be17f8b01a
myowngit1213 <cavin061213@outlook.com>
2022-03-02 15:43:50 +0200

    Fetched produts list from backend

--- Cantiin-React-MaterialUI/src/pages/products/index.tsx
+++ Cantiin-React-MaterialUI/src/pages/products/index.tsx
@@ -5,6 +5,16 @@ import type { creatingPageComponent } from "../../types";
 
 
 
+export async function getServerSideProps(context) {
+  const page = context.query.page || "1";
+  // Here we got the "page" query parameter from Context
+  // Default value is "1"
+
+  const res = await fetch(`https://cantiin.com/api/products/?page=${page}`);
+  const products = await res.json();
+  return {props: {products: products.results}}
+  // will be passed to the page component as props
+}
 
 
 
@@ -12,7 +22,8 @@ import type { creatingPageComponent } from "../../types";
 
 
 
-const ProductsList = (): creatingPageComponent=>{
+const ProductsList = ({products}): creatingPageComponent=>{
+  console.log(products);
   return (
   <>
     Products List
