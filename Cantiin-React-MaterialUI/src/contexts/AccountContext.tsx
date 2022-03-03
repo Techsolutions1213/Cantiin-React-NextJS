689cfd10a519ab2f5f22427642387b057ee3628d
myowngit1213 <cavin061213@outlook.com>
2022-03-03 19:53:02 +0200

    Created ccount Context

--- Cantiin-React-MaterialUI/src/contexts/AccountContext.tsx
+++ Cantiin-React-MaterialUI/src/contexts/AccountContext.tsx
@@ -1,6 +1,5 @@
-import {createContext, Component, useState} from 'react';
+import {createContext, useState} from 'react';
 import type { Context } from 'react';
-import type Items
 
 export interface AccountType {
     loggedIn: boolean, 
@@ -8,48 +7,22 @@ export interface AccountType {
 };
 
 
-
-export const AccountContext:Context<AccountType> = createContext({loggedIn:true});
-
+export const AccountContext:Context<AccountType> = 
+createContext({loggedIn:true, logOut:()=>{}, logIn:()=>{}});
 
 
 const AccountContextProvider:(any)=>JSX.Element = (props):JSX.Element=>{
-    let [state, setState] =  useState({loggedIn:false});
+    let [state, setState] = useState({loggedIn:false});
 
     const logOut:()=>void = ()=>{setState({loggedIn:false})};
     const logIn:()=>void = ()=>{setState({loggedIn:true})};
 
-
     return ( 
         <AccountContext.Provider value={
-            {...state, 
-            //toggleTheme:toggleTheme
-            
-            }}>
+            {...state, logOut, logIn}}>
             {props.children}
         </AccountContext.Provider> 
         );
-}
-
-
-class AccountContextProviderr extends Component {
-    state = { 
-        isLightTheme:true,
-        light:{syntax: "#555", ui: "#ddd", bg:"#eee"},
-        dark:{syntax: "#ddd", ui: "#333", bg:"#555"},
-    };
-
-    toggleTheme=()=>{
-        this.setState({isLightTheme:!this.state.isLightTheme});
-    }
-
-    render() { 
-        return ( 
-        <AccountContext.Provider value={
-            {...this.state, toggleTheme:this.toggleTheme}}>
-            {this.props.children}
-        </AccountContext.Provider> );
-    }
 }
  
 export default AccountContextProvider;
