4ffff644124eeff4614e2ace9e9047d216a3f144
myowngit1213 <cavin061213@outlook.com>
2022-03-05 20:54:13 +0200

    About Page now Ready (Missing Youtube Link)

--- Cantiin-React-MaterialUI/src/pages/about.tsx
+++ Cantiin-React-MaterialUI/src/pages/about.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import type { creatingPageComponent } from "../types";
 import { Container, Box, Typography } from "@mui/material";
+import Link from "next/link";
 
 export default function About() :creatingPageComponent {
   return (
     <Box>
       <ol>
-      <li><a href="">Github Repository and Documentation</a></li>
-      <li><a href="">Github Repository and Documentation</a></li>
-      <li><a href="">Github Repository and Documentation</a></li>
-      <li><a href="">Github Repository and Documentation</a></li>
-      <li><a href="">Github Repository and Documentation</a></li>
-      <li><a href="">Github Repository and Documentation</a></li>
-      <li><a href="">Github Repository and Documentation</a></li>
+        <li><Link href="https://github.com/OmarThinks/Cantiin-React-MaterialUI">Github Repository and Documentation</Link></li>
+        <li><Link href="https://www.cantiin-react.com/">Website (Deployed on AWS)</Link></li>
+        <li><Link href="">Youtube</Link></li>
+        <li><Link href="https://github.com/OmarThinks/Cantiin-React-MaterialUI">RESTful API backend Link</Link></li>
       </ol>
 
 
