770d47c8e874447eb31f20d4e435d1373277fe77
myowngit1213 <cavin061213@outlook.com>
2022-03-05 20:50:22 +0200

    Prepairing about page links

---
+++ Cantiin-React-MaterialUI/src/pages/about.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import type { creatingPageComponent } from "../types";
+import { Container, Box, Typography } from "@mui/material";
+
+export default function About() :creatingPageComponent {
+  return (
+    <Box>
+      <ol>
+      <li><a href="">Github Repository and Documentation</a></li>
+      <li><a href="">Github Repository and Documentation</a></li>
+      <li><a href="">Github Repository and Documentation</a></li>
+      <li><a href="">Github Repository and Documentation</a></li>
+      <li><a href="">Github Repository and Documentation</a></li>
+      <li><a href="">Github Repository and Documentation</a></li>
+      <li><a href="">Github Repository and Documentation</a></li>
+      </ol>
+
+
+
+    </Box>
+  )
+}
+
+About.header="About";
