534c9e42f27f620a70c030904946998ada7b4d55
myowngit1213 <cavin061213@outlook.com>
2022-03-07 17:57:15 +0200

    Home page updated

--- Cantiin-React-MaterialUI/src/pages/index.tsx
+++ Cantiin-React-MaterialUI/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React,{Fragment, useState} from "react";
+import React,{Fragment, useState, useEffect} from "react";
 
 import Pagination from '@mui/material/Pagination';
 import Box from '@mui/material/Box';
@@ -11,45 +11,52 @@ import ProductCard from "../components/ProductCard";
 
 /*Types*/
 import type { creatingPageComponent, productObject } from "../types";
+import { Typography } from "@mui/material";
+
+
+
+
+const ProductsList = (): creatingPageComponent=>{
+
+  const router:any = useRouter();
+  console.log(router);
+  const currentPage:string = (parseInt(router.query.page) || 1).toString();
+  console.log(currentPage);
+
+  let [loading, setLoading]=useState(true);
+  
+  let [productsObject, setProductsObject]:
+  [productsObject:{products:productObject[], pagesCount:number}, setProductsObject:any] = 
+  useState({products:[], pagesCount:1});
+
+    
+
+  useEffect(()=>{
+    
+    (async()=>{
+      const res = await fetch(`https://cantiin.com/api/products/?page=${currentPage}`);
+      const products:{
+        count:number,
+        next:string|null, previous:string|null,
+        results:productObject[]
+      } = await res.json();
+    
+      let pagesCount:number =parseInt((products.count/10).toPrecision(1))+1;
+      
+      setProductsObject({products:products.results, pagesCount});
+      setLoading(false);
+
+    })()
+  }, [currentPage]);
 
 
 
 
 
 
-export async function getServerSideProps(context) {
-  const page:string = (parseInt(context.query.page) || 1).toString();
-  // Here we got the "page" query parameter from Context
-  // Default value is "1"
-
-  const res = await fetch(`https://cantiin.com/api/products/?page=${page}`);
-  const products:{
-    count:number,
-    next:string|null, previous:string|null,
-    results:productObject[]
-  } = await res.json();
-
-  let pagesCount:number =parseInt((products.count/10).toPrecision(1))+1;
 
 
 
-  return {props: {products: products.results, fillProductsObject:products, pagesCount, 
-  currentPage:page
-  }}
-  // will be passed to the page component as props
-}
-
-
-
-
-
-
-
-const ProductsList = ({products, fillProductsObject, pagesCount, currentPage}:
-  {products:productObject[],fillProductsObject:any, pagesCount:number, currentPage:string}): 
-  creatingPageComponent=>{
-
-  const router = useRouter();
 
   const handleChange = (event: React.ChangeEvent<unknown>, pageNumber: number):void => {
     event.preventDefault()
@@ -59,12 +66,51 @@ const ProductsList = ({products, fillProductsObject, pagesCount, currentPage}:
     });
   };
   
-  let productsComponent:JSX.Element[] = products.map((product:productObject)=>{
-    return(
-    <Fragment key={product.id}>
-      <ProductCard product={product}/>
-    </Fragment>
-  )});
+
+  console.log("productsObject",productsObject);
+
+
+  if(productsObject.products===undefined){
+    setLoading(true);
+    setProductsObject({products:[], pagesCount:1});
+
+    router.push({
+      pathname: '/',
+      query: { page: 1 },
+    });
+    return <></>;
+  }
+
+
+
+  let productsComponent:JSX.Element =
+  loading?<Typography align="center" variant="h3">Loading...</Typography>:
+  <>
+    {productsObject.products.map((product:productObject)=>{
+      return(
+      <Fragment key={product.id}>
+        <ProductCard product={product}/>
+      </Fragment>
+    )})}
+  </>
+  ;
+  
+  
+
+
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
   return (
   <>
       <Box
@@ -72,7 +118,7 @@ const ProductsList = ({products, fillProductsObject, pagesCount, currentPage}:
         justifyContent="center"
         marginBottom={3}
       >
-        <Pagination count={pagesCount} color="secondary" size="large"
+        <Pagination count={productsObject.pagesCount} color="secondary" size="large"
         onChange={handleChange} page={parseInt(currentPage)}
         showFirstButton showLastButton/>
       </Box>
@@ -82,7 +128,7 @@ const ProductsList = ({products, fillProductsObject, pagesCount, currentPage}:
         justifyContent="center"
         mt={4}
       >
-        <Pagination count={pagesCount} color="secondary" size="large"
+        <Pagination count={productsObject.pagesCount} color="secondary" size="large"
         onChange={handleChange} page={parseInt(currentPage)}
         showFirstButton showLastButton/>
       </Box>
