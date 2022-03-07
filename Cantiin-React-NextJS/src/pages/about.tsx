e793121bca501c10e254ebe26e46c39f73ac4de5
myowngit1213 <cavin061213@outlook.com>
2022-03-08 00:05:01 +0200

    Updated link in about page

--- Cantiin-React-NextJS/src/pages/about.tsx
+++ Cantiin-React-NextJS/src/pages/about.tsx
@@ -7,10 +7,10 @@ export default function About() :creatingPageComponent {
   return (
     <Box>
       <ol>
-        <li><Link href="https://github.com/OmarThinks/Cantiin-React-MaterialUI">Github Repository and Documentation</Link></li>
+        <li><Link href="https://github.com/OmarThinks/Cantiin-React-NextJS">Github Repository and Documentation</Link></li>
         <li><Link href="https://www.cantiin-react.com/">Website (Deployed on AWS)</Link></li>
-        <li><Link href="">Youtube</Link></li>
-        <li><Link href="https://github.com/OmarThinks/Cantiin-React-MaterialUI">RESTful API backend Link</Link></li>
+        <li><Link href="https://www.youtube.com/watch?v=sBj2vWlMLfc">Youtube</Link></li>
+        <li><Link href="https://cantiin.com/api/">RESTful API backend Link</Link></li>
       </ol>
 
 
